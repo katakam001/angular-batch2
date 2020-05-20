9851e03e0521424469b730947b3fb05f2bb06e4c
katakam001 <katakam001@gmail.com>
2020-05-20 21:18:10 +0530

    Added Event BInding changes

--- src/app/item-component/item-component.component.ts
+++ src/app/item-component/item-component.component.ts
@@ -8,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class ItemComponentComponent implements OnInit {
   title = 'Create a Account2';
   itemImageUrl="../assets/images/Spring-Promo.jpg"
-  isUnchanged=true;
+  isUnchanged=false;
   constructor() { }
+  keyUp:String;
 
   ngOnInit(): void {
+  }
+  display(){
+    console.log("Button is pressed");
+    alert("this is button");
+  }
+  secondDisplay($event){
+    console.log($event);
+  }
+  onKey(event: any){
+   console.log(event.target.value);
+   this.keyUp=event.target.value;
+   console.log(this.keyUp.length);
   }
 
 }
