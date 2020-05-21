c18200ac36e6b81eee34d7fa7e0f56f912749013
katakam001 <katakam001@gmail.com>
2020-05-21 09:55:51 +0530

    Event Binding and Style Binding changes.

--- src/app/item-component/item-component.component.ts
+++ src/app/item-component/item-component.component.ts
@@ -11,20 +11,33 @@ export class ItemComponentComponent implements OnInit {
   isUnchanged=false;
   constructor() { }
   keyUp:String;
-
+  amar="xx-large";
+  styleExpr="font-size:xx-large;color:red;"
   ngOnInit(): void {
   }
   display(){
     console.log("Button is pressed");
     alert("this is button");
   }
-  secondDisplay($event){
-    console.log($event);
+  secondDisplay(event: any){
+    console.log(event);
   }
   onKey(event: any){
    console.log(event.target.value);
    this.keyUp=event.target.value;
    console.log(this.keyUp.length);
   }
-
+  tabout(event:any){
+    console.log(event.target.value);
+  }
+  tabout1(event:any){
+    console.log(event.target.value);
+  }
+  selected(event:any){
+  alert(event.value);
+  this.amar="large";
+  }
+  unselected(event:any){
+   alert(event.value);
+  }
 }
