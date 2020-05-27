d7dc64077f6df6fb9fcc6f19bee9ba3a1f2d886a
katakam001 <katakam001@gmail.com>
2020-05-27 22:08:14 +0530

    Added the structural directives change like ngIf and ngFor

--- src/app/item-component/item-component.component.ts
+++ src/app/item-component/item-component.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SafeMethodCall } from '@angular/compiler';
+import { Food } from '../model/Food';
 
 @Component({
   selector: 'app-item-component',
@@ -10,6 +11,7 @@ export class ItemComponentComponent implements OnInit {
   title = 'Create a Account2';
   itemImageUrl="../assets/images/Spring-Promo.jpg"
   currentStyles: {};
+  currentClasses:{};
   keyUp:String;
   amar="xx-large";
   styleExpr="font-size:xx-large;color:red;"
@@ -18,10 +20,18 @@ export class ItemComponentComponent implements OnInit {
   canSave=false;
   isSpecial=false;
   isUnchanged=false;
+  isDisplay=false;
   hello:string;//ng model
+  items = ["bhaskar","amar","rajesh","naren"];
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
   constructor() { }
   ngOnInit(): void {
     this.setCurrentStyles();
+    this.setCurrentClass();
   }
   display(){
     console.log("Button is pressed");
@@ -60,4 +70,14 @@ export class ItemComponentComponent implements OnInit {
     };
   }
 
+  setCurrentClass() {
+    // CSS styles: set per current state of component properties
+    this.currentClasses = {
+      'color-red-class': this.canSave,
+      'position-right-class': this.isUnchanged,
+    };
+  }
+  foodDisplay(value){
+    console.log(value);
+  }
 }
